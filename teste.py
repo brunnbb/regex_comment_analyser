e9686265dd64d5c0979d8e5ab498e243c4fcb937
Bruno <brunobbianchini@gmail.com>
2025-04-28 09:15:40 -0300

    Organizando trabalho

--- teste.py
+++
@@ -1,9 +0,0 @@
-from extrator_e_analisador.analisador import Analisador
-
-a = ['Filme não é bom mas incrível',
-     'Filme ruim',
-     'Filme mediocre',
-     'Filme com bons momentos mas genérico']
-analisador = Analisador() 
-r = analisador.analisar_paginas(a)
-print(r)
